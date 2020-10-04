8908825a9fdcb6b3a5d06922f78c13a90aaa690c
Saurabh <saurabh.nit20@gmail.com>
2020-10-04 23:08:01 +0530

    nlog

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+
+namespace ConsoleApp3
+{
+    class Addnumbers
+    {
+        private readonly NLog nLog = new NLog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            if(firstNumber == 0 || secondNumber ==0)
+            {
+                nLog.LogDebug("Debug Unsuccesful");
+                nLog.LogError("Expecting NULL values");
+                nLog.LogWarn("firstNumber or secondNumber should not equla to 0");
+
+            }
+            int result = firstNumber + secondNumber;
+            nLog.LogDebug("Debug succesful");
+            nLog.LogInfo("Sum is " + result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Addnumbers add = new Addnumbers();
+            Console.WriteLine("Result is :" + "" + add.Sum(a, b));
+        }
+    }
+}
